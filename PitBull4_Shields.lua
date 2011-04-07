cbee96c5e5ba0f57b8259b0fb957111035ee6787
Craig R. Hughes <craig.git@rungie.com>
2011-04-07 12:24:52 +0900

    Enable localization of display strings; will need to deal with localized spell names at some point

--- PitBull4_Shields.lua
+++ PitBull4_Shields.lua
@@ -12,8 +12,8 @@ local L = PitBull4.L
 local PitBull4_Shields = PitBull4:NewModule("Shields")
 
 PitBull4_Shields:SetModuleType("bar_provider")
-PitBull4_Shields:SetName("Shields")
-PitBull4_Shields:SetDescription("Display bars for remaining amount of shielding on the unit")
+PitBull4_Shields:SetName(L("Shields"))
+PitBull4_Shields:SetDescription(L("Display bars for remaining amount of shielding on the unit"))
 PitBull4_Shields:SetDefaults({
 	enabled = false,
 	first = true,
@@ -129,8 +129,8 @@ end
 PitBull4_Shields:SetLayoutOptionsFunction(function(self)
 	return "hide_empty", {
 		type = "toggle",
-		name = "Hide empty bar",
-		desc = "Check this to hide the Shields bar if empty",
+		name = L("Hide empty bar"),
+		desc = L("Check this to hide the Shields bar if empty"),
 		get = function(info)
 			local bar_db = PitBull4.Options.GetBarLayoutDB(self)
 			return bar_db and bar_db.hide
