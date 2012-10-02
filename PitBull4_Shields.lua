80a2c441ff2718fa536c13cb3c07455e387a051c
Craig R. Hughes <craig.git@rungie.com>
2012-10-01 18:52:06 -0700

    Add Fury Ward for warlocks

--- PitBull4_Shields.lua
+++ PitBull4_Shields.lua
@@ -65,6 +65,7 @@ PitBull4_Shields_combatFrame.shields = {
             -- Warlock
             [1454] = { max = {}, cur = {} }, -- Life Tap (debuff)
             [6229] = { max = {}, cur = {} }, -- Twilight Ward
+            [119839] = { max = {}, cur = {} }, -- Fury Ward (Dark Apotheosis)
             [108416] = { max = {}, cur = {} }, -- Sacrificial Pact
             [110913] = { max = {}, cur = {} }, -- Dark Bargain
 
